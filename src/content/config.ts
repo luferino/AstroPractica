18cf7c79be003ef33163f6cdec325293459aa858
Luis Benegas <luisbene12@gmail.com>
2026-05-05 20:49:10 -0300

    no c

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,7 @@
 import { defineCollection } from "astro:content";
-import { z } from "astro/zod";
-import { glob } from "astro/loaders";
+import { z } from "astro/zod";  
 
 const books = defineCollection({
-    loader: glob({ pattern: "./**/*.md", base: "src/content/books" }),
     schema: z.object({
         title: z.string(),
         author: z.string(),
