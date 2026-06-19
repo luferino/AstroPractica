74ce0eb587bb3569302e85d0e5267ae44d42faa8
Luis Benegas <luisbene12@gmail.com>
2026-06-18 21:14:37 -0300

    pruebas

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,23 @@ import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 
+import netlify from '@astrojs/netlify';
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
+  output: 'server',
+
   env: { 
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({default: true, context:"server", access: 'public'}),
       SCORE_API_ENDPOINT: envField.string({context:"server", access: 'public'}),
     }
-}
+},
+
+  adapter: netlify()
 });
